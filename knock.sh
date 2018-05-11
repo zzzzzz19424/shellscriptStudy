9f1eba7f4953cb14d34e088a8e8949a8930339b7
cwyang41 <cwyang41@gmail.com>
2018-05-12 01:35:00 +0800

    switch window to input click

--- knock.sh
+++ knock.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-WID="$(xdotool search --name '0PKV1')"
-xdotool windowactivate ${WID}
+ClientWID="$(xdotool search --name '0PKV1')"
+HostWID="$(xdotool search --name 'reginapc')"
 
 
-echo "${WID}"
 
+while :
+do
 
+	xdotool windowactivate ${ClientWID}
+    xdotool mousemove 1300 500 click 1
+    sleep 1.2
+    xdotool windowactivate ${HostWID}
+
+    read -t 0.01 -n 1 key
+    if [[ $key = q ]]
+    then
+        break
+    fi
+
+done
 
